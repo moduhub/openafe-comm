07d0d4b4341b8d4d205a0ffd2dcd5af176242f77
ig-66 <igor@moduhub.com>
2023-08-09 16:20:50 -0300

    Added error codes header file

---
+++ data_format_tests/openAFE_Error_Codes.h
@@ -0,0 +1,9 @@
+#ifndef _OPENAFE_ERROR_CODES_
+#define _OPENAFE_ERROR_CODES_
+
+#define ERROR_INVALID_COMMAND -1 // Command is invalid.
+#define ERROR_PARAM_OUT_BOUNDS -2 // One or more parameters are out of bounds.
+#define ERROR_PARAM_MISSING -3 // One or more parameters are missing.
+
+
+#endif //_OPENAFE_ERROR_CODES_
