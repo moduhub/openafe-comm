3ef9c1e92a919852a08cd21b59883507265789d5
EduardoPaz01 <eduardopazputti@gmail.com>
2025-12-22 18:39:19 -0300

    [misc] Fixed the EIS parsing to receive only the 4 parameters configured in the AFE

--- src/interpreter/parse-command/parseEIS.cpp
+++ src/interpreter/parse-command/parseEIS.cpp
@@ -3,14 +3,13 @@
 void _parseEISParams(command_t *pCommandParams, String pCommandString){
 	String tParamArray[5];
 
-	separateCommandParameters(tParamArray, pCommandString, ',', 5);
+	separateCommandParameters(tParamArray, pCommandString, ',', 4);
 
 	pCommandParams->id = CMDID_EIS;
 	pCommandParams->settlingTime = tParamArray[0].toInt();
 	pCommandParams->startingOmega = tParamArray[1].toInt();
 	pCommandParams->endingOmega = tParamArray[2].toInt();
 	pCommandParams->stepForADecade = tParamArray[3].toInt();
-	pCommandParams->samplesPerFrequency = tParamArray[4].toInt();
   
 	return;
 }
